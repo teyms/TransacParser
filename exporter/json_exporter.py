8d640027fd85cb3c0e424a554c5215c4f9771cc5
teyms <teymingsheng@gmail.com>
2025-06-04 21:19:59 +0800

    small adjustment ouput filename

--- exporter/json_exporter.py
+++ exporter/json_exporter.py
@@ -6,11 +6,10 @@ from datetime import datetime
 timestamp = datetime.now().strftime("%Y%m%d%H%M%S")  # Now includes seconds
 default_filename = f"output_{timestamp}.json"
 def json_exporter(df, filename=default_filename):
-    if filename is None:
-        filename = default_filename
+    filename += f'{timestamp}.json'  # Ensure the filename ends with .json
     # Convert to JSON string
     json_str = df.to_json(orient='records')
-    print('\n'+json_str)
+    # print('\n'+json_str)
     # Save to a JSON file
     with open(f'./data/processed/{filename}', 'w') as f:
         json.dump(json.loads(json_str), f, indent=4)  # indent for pretty formatting
