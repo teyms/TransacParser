076a4cb16830850b139de4e1d2d47c6902655baf
teyms <teymingsheng@gmail.com>
2025-06-07 17:57:02 +0800

    To support multiple files to Parse in once, instead of 1 at a time

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import json
-from utils.helpers import apply_header_mapping
+from utils.helpers import apply_header_mapping, load_config
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
 from parsers import parse_pdf
 from exporter import json_exporter
 
 load_dotenv()  # Load environment variables from .env
-
+GLOBAL_CONFIG = load_config('config.json')
 def output_manager(df, output_type="json", filename=None):
     """
     Manages the output format of the DataFrame.
@@ -20,30 +20,50 @@ def output_manager(df, output_type="json", filename=None):
             raise ValueError(f"Unsupported output type: {output_type}")
 
 if __name__ == "__main__":
-    input_file = os.getenv("INPUT_FILE", "xxx.pdf")  # Fallback to "default.csv"
-    bank_name = os.getenv("BANK_NAME", "DBS")           # Fallback to "DBS"
+#     input_file = os.getenv("INPUT_FILE", "xxx.pdf")  # Fallback to "default.csv"
+#     bank_name = os.getenv("BANK_NAME", "DBS")           # Fallback to "DBS"
     password = os.getenv("PDF_PASSWORD", 'password')  # Optional password for PDF files
-    country_code = os.getenv("COUNTRY_CODE", "MY")  # Fallback to "MY"
-    country_code = os.getenv("COUNTRY_CODE", "SG")  # Fallback to "SG"
-    bank_code = os.getenv("BANK_CODE", "TNG")  # Fallback to "TNG"
-    bank_code = os.getenv("BANK_CODE", "PBB")  # Fallback to "PBB"
-    bank_code = os.getenv("BANK_CODE", "DBS")  # Fallback to "DBS"
-    bank_code = os.getenv("BANK_CODE", "CIMB")  # Fallback to "DBS"
+#     country_code = os.getenv("COUNTRY_CODE", "MY")  # Fallback to "MY"
+#     country_code = os.getenv("COUNTRY_CODE", "SG")  # Fallback to "SG"
+#     bank_code = os.getenv("BANK_CODE", "TNG")  # Fallback to "TNG"
+#     bank_code = os.getenv("BANK_CODE", "PBB")  # Fallback to "PBB"
+#     bank_code = os.getenv("BANK_CODE", "DBS")  # Fallback to "DBS"
+#     bank_code = os.getenv("BANK_CODE", "CIMB")  # Fallback to "CIMB"
+#     output_type = os.getenv("OUTPUT_TYPE", "json")  # Fallback to "json"
+#     # output_filename = os.getenv("OUTPUT_FILENAME", None)  # Fallback to "json"
 
+# ###########################################################################
 
-    output_type = os.getenv("OUTPUT_TYPE", "json")  # Fallback to "json"
-    # output_filename = os.getenv("OUTPUT_FILENAME", None)  # Fallback to "json"
+    folder_path = load_config('folder_paths.json')    
+    source_paths = folder_path.get('folder_paths', {}).get("source", {})
 
-    print(f"PDF loaded successfully: {os.path.exists(input_file)}")
+    prev_month_date = (datetime.now().replace(day=1) - timedelta(days=1)).strftime("%Y%m")
+    yyyydd = prev_month_date if GLOBAL_CONFIG["read_date_format"]["format"] is None else GLOBAL_CONFIG["read_date_format"]["format"]
 
-    df = parse_pdf(input_file, bank_code, country_code, password=password)
-    print(f"\n{df}")
-    
-    # Apply header mapping
-    df = apply_header_mapping(df, bank_code, country_code, mapping_path='bank_mappings.json')
-    # print(df)
-    # # Convert to JSON string
-    # json_str = df.to_json(orient='records')
-    # print('\n'+json_str)
-    output_filename = f"{bank_code}_{country_code}_output_"  # Default filename
-    output_manager(df, output_type=output_type, filename=output_filename)
+    print(f"Prev date: {yyyydd}")
+    # raise KeyError # for testing purposes
+    for key, value in source_paths.items():
+        bank_country_code = f"{key.upper()}"
+        extension_priority = GLOBAL_CONFIG["read_extension_priority"]
+        # set the `filename` extension based on the priority, and if file available then use it and skip for the rest extensions, otherwise continue to the next extension
+        # the extension should follow the array get from `read_extension_priority` in the config.json
+        for ext in extension_priority:
+            filename = f"{bank_country_code}_{yyyydd}.{ext}"
+            # check if the file exists with the current extension
+            input_file = os.path.join(value, filename)
+            print(f"Checking file: {input_file}")
+            if os.path.exists(input_file):
+                print(f"Using file: {input_file}")
+                break
+        output_type = GLOBAL_CONFIG["output"]["file_format"]
+        # df = parse_pdf(input_file, bank_code, country_code, password=password)
+        df = parse_pdf(input_file, bank_country_code, password=password)
+        print(f"\n{df}")
+        
+        bank_code = bank_country_code.split('_')[0]  # Extract bank code from the key
+        country_code = bank_country_code.split('_')[1]  # Extract country code from the key
+        # Apply header mapping
+        df = apply_header_mapping(df, bank_code, country_code, mapping_path='bank_mappings.json')
+
+        output_filename = f"{bank_code}_{country_code}_output_"  # Default filename
+        output_manager(df, output_type=output_type, filename=output_filename)            
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import pandas as pd
@@ -8,18 +7,9 @@ def apply_header_mapping(df, bank_code, country_code, mapping_path='bank_mapping
     Loads a column mapping from JSON and renames DataFrame columns accordingly.
     """
     try:
-        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # This gets the root 'transacparser/' dir
-        # print(f"Base directory: {base_dir}")
-        mapping_path = os.path.join(base_dir, 'configs', mapping_path)
-        # print(f"Mapping file path: {mapping_path}")
+        mapping_json = load_config(mapping_path)
 
-        if not os.path.exists(mapping_path):
-            raise FileNotFoundError(f"Mapping file not found: {mapping_path}")
-        
-        with open(mapping_path, 'r') as f:
-            column_mapping = json.load(f)
-
-        column_mapping = column_mapping.get('bank_mappings', {}).get(bank_code, {}).get(country_code, {})
+        column_mapping = mapping_json.get('bank_mappings', {}).get(bank_code, {}).get(country_code, {})
         # print(f"Column mapping loaded: {column_mapping}")
         # Optionally: Keep only columns defined in the mapping
         df = df[[col for col in df.columns if col in column_mapping]]
@@ -31,3 +21,16 @@ def apply_header_mapping(df, bank_code, country_code, mapping_path='bank_mapping
         return pd.DataFrame()  # Return the empty DataFrame if mapping fails
 
     return df.rename(columns=column_mapping)
+
+def load_config(name):
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # This gets the root 'transacparser/' dir
+    # print(f"Base directory: {base_dir}")
+    config_path = os.path.join(base_dir, 'configs', name)
+    # print(f"Mapping file path: {name}")
+
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Mapping file not found: {config_path}")
+    
+    with open(config_path, 'r') as f:
+        json_content = json.load(f)
+    return json_content
