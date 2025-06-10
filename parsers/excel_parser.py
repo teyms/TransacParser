b58e7a7cdda0596f7f05db806f3c5267c8017daa
teyms <teymingsheng@gmail.com>
2025-06-10 19:50:07 +0800

    add excel parse for DBS singapore

--- parsers/excel_parser.py
+++ parsers/excel_parser.py
@@ -10,7 +10,22 @@ load_dotenv()  # Load environment variables from .env
 def parse_excel(input_file, bank_country_code, password=None):
     # print(f"bank_country_code: {bank_country_code}")
     # df = pd.read_excel(input_file, engine='openpyxl')
-    df = pd.read_csv(input_file)
+    match bank_country_code:
+        case "DBS_SG":
+            headers = pd.read_csv(input_file, skiprows=19, engine='python', nrows=1).columns.tolist() # Get headers from the first row
+            # print(f"DBS_SG headers: {headers}")
+            df = pd.read_csv(
+                    input_file,
+                    skiprows=19,
+                    names=headers,
+                    header=None,
+                    engine="python",
+                    usecols=range(len(headers)),  # ignore extra columns
+                    skipinitialspace=True
+            )
+        case _:
+            df = pd.read_csv(input_file)
+
     headers = df.columns.tolist()  # Get headers from the DataFrame
     headers = [header.replace('\n', '_')
                     .replace(' ', '_')
